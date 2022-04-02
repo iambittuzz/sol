73d056f5490e902e8bd412135d1b5885ceb76d25
iambittuzz <89698917+iambittuzz@users.noreply.github.com>
2022-04-02 17:32:40 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://user500:9xFx_e!mpxNbcK!@cluster0.5u03a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'5164793148:AAFxhvkFQ_Adebk4KxaaUz9-og4dTtHrP7w',//bot token
+
+bot_name:'litcoinltcautopayvbot',//bot username without @
+
+bot_admin: 2087872161,// admin telegram id
+
+channelsList : ['@BscLooters'],// channels list
+
+reffer_bonus:100,// refferal bonus amount
+
+min_wd:0.0000005,//min withdrawal amount
+
+daily_bonus:0.0000005,// daily bonus amount
+
+currency:'LTC', //bot cuurency
+
+cb_api_key:'bVNzfQAazErlmVO7',//coinbase api key
+
+cb_api_secret:'86PlE38WSFBVW5EWuwrIAco65MdMWBc9',// coinbase secret key
+
+cb_account_id:'d243c4e9-268c-52c0-b70f-b8c42f88f7c9',// counbase account id for the bot currency
+
+payment_channel:'@bsclooters'// payment channel username
+
+}
